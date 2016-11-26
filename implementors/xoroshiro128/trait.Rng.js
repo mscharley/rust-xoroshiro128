428e9bd59e8f6ee9dbabccbf5ffc423401c1c09d
CircleCI <matt.scharley+circleci@gmail.com>
2016-11-26 04:37:55 +0000

    Updated documentation build.

--- implementors/xoroshiro128/trait.Rng.js
+++ implementors/xoroshiro128/trait.Rng.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["xoroshiro128"] = ["impl <a class='trait' href='xoroshiro128/trait.Rng.html' title='xoroshiro128::Rng'>Rng</a> for <a class='struct' href='xoroshiro128/struct.XoroshiroRng.html' title='xoroshiro128::XoroshiroRng'>XoroshiroRng</a>","impl <a class='trait' href='xoroshiro128/trait.Rng.html' title='xoroshiro128::Rng'>Rng</a> for <a class='struct' href='xoroshiro128/struct.SplitMixRng.html' title='xoroshiro128::SplitMixRng'>SplitMixRng</a>",];
+implementors["xoroshiro128"] = ["impl <a class='trait' href='xoroshiro128/trait.Rng.html' title='xoroshiro128::Rng'>Rng</a> for <a class='struct' href='xoroshiro128/struct.Xoroshiro128Rng.html' title='xoroshiro128::Xoroshiro128Rng'>Xoroshiro128Rng</a>","impl <a class='trait' href='xoroshiro128/trait.Rng.html' title='xoroshiro128::Rng'>Rng</a> for <a class='struct' href='xoroshiro128/struct.XorShift1024Rng.html' title='xoroshiro128::XorShift1024Rng'>XorShift1024Rng</a>","impl <a class='trait' href='xoroshiro128/trait.Rng.html' title='xoroshiro128::Rng'>Rng</a> for <a class='struct' href='xoroshiro128/struct.SplitMix64Rng.html' title='xoroshiro128::SplitMix64Rng'>SplitMix64Rng</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
