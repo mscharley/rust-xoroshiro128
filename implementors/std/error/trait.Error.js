330840d8c25ddf50f827a60e7ae00ce49bfae027
CircleCI <matt.scharley+circleci@gmail.com>
2018-01-19 05:14:27 +0000

    Updated documentation build.

--- implementors/std/error/trait.Error.js
+++ implementors/std/error/trait.Error.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["xoroshiro128"] = [];
+implementors["xoroshiro128"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='https://docs.rs/rand/0.3/rand/jitter/enum.TimerError.html' title='rand::jitter::TimerError'>TimerError</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
