f1ba9909e8d3ea95025fba60abf3bf28ece1430f
CircleCI <matt.scharley+circleci@gmail.com>
2019-11-03 01:26:35 +0000

    Updated documentation build.

---
+++ implementors/xoroshiro128/trait.RngCore.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["xoroshiro128"] = [{text:"impl <a class=\"trait\" href=\"xoroshiro128/trait.RngCore.html\" title=\"trait xoroshiro128::RngCore\">RngCore</a> for <a class=\"struct\" href=\"xoroshiro128/struct.Xoroshiro128Rng.html\" title=\"struct xoroshiro128::Xoroshiro128Rng\">Xoroshiro128Rng</a>",synthetic:false,types:["xoroshiro128::xoroshiro::Xoroshiro128Rng"]},{text:"impl <a class=\"trait\" href=\"xoroshiro128/trait.RngCore.html\" title=\"trait xoroshiro128::RngCore\">RngCore</a> for <a class=\"struct\" href=\"xoroshiro128/struct.XorShift1024Rng.html\" title=\"struct xoroshiro128::XorShift1024Rng\">XorShift1024Rng</a>",synthetic:false,types:["xoroshiro128::xorshift::XorShift1024Rng"]},{text:"impl <a class=\"trait\" href=\"xoroshiro128/trait.RngCore.html\" title=\"trait xoroshiro128::RngCore\">RngCore</a> for <a class=\"struct\" href=\"xoroshiro128/struct.SplitMix64Rng.html\" title=\"struct xoroshiro128::SplitMix64Rng\">SplitMix64Rng</a>",synthetic:false,types:["xoroshiro128::splitmix::SplitMix64Rng"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- xoroshiro128/sidebar-items.js
+++ xoroshiro128/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["SplitMix64Rng",""],["XorShift1024Rng",""],["Xoroshiro128Rng",""]],"trait":[["Rand","A type that can be randomly generated using an `Rng`."],["Rng","A random number generator."],["SeedableRng","A random number generator that can be explicitly seeded to produce the same stream of randomness multiple times."]]});
+initSidebarItems({"struct":[["SplitMix64Rng",""],["XorShift1024Rng",""],["Xoroshiro128Rng",""]],"trait":[["RngCore","The core of a random number generator."],["SeedableRng","A random number generator that can be explicitly seeded."]]});
