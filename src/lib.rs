77de886d8025f35bd11b595184ee64b54a1ea7ec
Matthew Scharley <matt@scharley.me>
2020-06-05 13:22:26 +1000

    feat: implement no_std as a feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 //! Utilities for generating psuedo-random numbers quickly.
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 #![doc(html_logo_url = "https://www.rust-lang.org/logos/rust-logo-128x128-blk.png",
        html_favicon_url = "https://www.rust-lang.org/favicon.ico",
        html_root_url = "https://mscharley.github.io/rust-xoroshiro128/")]
@@ -22,5 +24,5 @@ pub use splitmix::SplitMix64Rng;
 mod tests;
 
 fn u64_from_sl(s: &[u8]) -> u64 {
-       u64::from_le_bytes([s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7]])
+  u64::from_le_bytes([s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7]])
 }
